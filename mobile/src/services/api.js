a76efe00e5ec1d6047f94aacfa8b47fce44cfa85
Christyellen <christyellenn@hotmail.com>
2020-03-27 20:56:42 -0300

    remoção de comentário

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333' //substituir 'localhost' pelo ip exibido após a execução do expo
+    baseURL: 'http://localhost:3333'
 })
 
 export default api;
